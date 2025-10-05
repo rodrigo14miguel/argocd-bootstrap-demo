aa1a46e94115181a4484e6c2abf0031c13390f73
rmf <rodrigo14miguel@gmail.com>
2025-10-05 15:44:37 +0100

    update bootstrap resources

--- terraform/region1/argocd-bootstrap.tf
+++ terraform/region1/argocd-bootstrap.tf
@@ -1,6 +1,19 @@
-resource "kubernetes_manifest" "argocd-resources" {
-  for_each = fileset("../../bootstrap/${var.region}", "*.yaml")
+resource "kubernetes_manifest" "argocd-projects" {
+  for_each = fileset("../../bootstrap/${var.region}/", "project-*.yaml")
   manifest = yamldecode(file("../../bootstrap/${var.region}/${each.value}"))
 
   depends_on = [ kustomization_resource.p2 ]
+}
+
+resource "time_sleep" "wait_argo_deploy" {
+  depends_on = [kubernetes_manifest.argocd-projects]
+
+  create_duration = "90s"
+}
+
+resource "kubernetes_manifest" "argocd-apps" {
+  for_each = fileset("../../bootstrap/${var.region}/", "app-*.yaml")
+  manifest = yamldecode(file("../../bootstrap/${var.region}/${each.value}"))
+
+  depends_on = [ time_sleep.wait_argo_deploy ]
 }
